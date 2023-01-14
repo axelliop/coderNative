be9235177d3621a2c913e9d117027a17f68cc71a
axelliop <axelliop@gmail.com>
2023-01-13 22:51:45 -0300

    cambios

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+/* ------------------------ MODALVISIBLE IS NOT DEFINED SOLUCIONAR, "SOLUCIONADO" ------------------------ */
+
+
+/* ------------------------ REVEER LA CLASE Y ANOTAR TODO. NUEVAMENTE ------------------------ */
+
+
+
+/* siempre colocar la propiedad TITLE en button, textinput para escribir un texto */
+
+import { Button, FlatList, StyleSheet, Text, View } from "react-native"
+import React, { useState } from "react"
+
+import AddItem from "./src/components/AddItem"
+import Modal from "./src/components/Modal"
 
 export default function App() {
+  const [textItem, setTextItem] = useState("")
+  const [list, setList] = useState([])
+  const [itemSelected, setItemSelected] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const onHandleChangeItem = text => {
+    setTextItem(text)
+  }
+
+  const addItem = () => {
+    setList(prevState => [...prevState, textItem])
+    setTextItem("")
+  }
+
+
+  /* ----------- SETEO EL itemSelected con el elemento item ---------------- */
+  const handleModal = item => {
+    setItemSelected(item)
+    setModalVisible(true)
+  }
+
+  /* ----- PARA BORRAR EL ITEM ----- */
+  const onHandleDelete = item => {
+    console.log(item)
+    setList(prevState => prevState.filter(element => element !== item))
+    setModalVisible(!modalVisble)
+  }
+
+
+  /* -------- RENDEREO EL ITEM (LO MUESTRO A LA VISTA COMO YO QUIERO CON EL ESTILO QUE QUIERO Y LUEGO LO MUESTRO) */
+  const renderItem = ({ item }) => (
+    <View style={styles.renderItemStyle}>
+      <Text>{item}</Text>
+      <Button title="Edit" onPress={() => handleModal(item)} />
+    </View>
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Shopping List</Text>
+        <AddItem
+          onChange={onHandleChangeItem}
+          textValue={textItem}
+          onAddItem={addItem}
+        />
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={list}
+          renderItem={renderItem}
+          keyExtractor={item => item}
+        />
+      </View>
+      <Modal
+      /* --------- AGREGO LA VARIABLE DE LOS ESTADOS, DE ESTA FORMA DESPUES LO PUEDO PASAR EN PROPS EN MODAL.JS -------------- */
+        isVisible={modalVisible}
+        itemSelected={itemSelected}
+        actionDeleteItem={() => onHandleDelete(itemSelected)}
+        onDismissModal={setModalVisible}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E7EAF2",
   },
-});
+  titleContainer: {
+    height: 200,
+    paddingHorizontal: 30,
+    paddingTop: 80,
+  },
+  title: {
+    marginBottom: 30,
+    fontSize: 40,
+    fontWeight: "500",
+    color: "#1E283C",
+  },
+  listContainer: {
+    flex: 2,
+    marginHorizontal: 30,
+    marginTop: 40,
+    padding: 3,
+  },
+  renderItemStyle: {
+    height: 60,
+    flexDirection: "row",
+    marginBottom: 25,
+    backgroundColor: "white",
+    borderRadius: 10,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 2,
+    elevation: 3,
+  },
+})
---
+++ src/components/Modal.js
@@ -0,0 +1,57 @@
+//RFNES
+
+// ---- al importar el modal le podemos cambiar el nombre, en este caso NewModal
+
+import { Button, Modal as NewModal, StyleSheet, Text, View } from "react-native"
+
+import React from "react"
+
+const Modal = ({
+  isVisible,
+  actionDeleteItem,
+  itemSelected,
+  onDismissModal,
+}) => {
+  return (
+    <NewModal animationType="fade" transparent={true} visible={isVisible}>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalStyle}>
+          <Text style={styles.modalTextStyle}>{itemSelected}</Text>
+          <Button title="Delete" onPress={() => actionDeleteItem()} />
+
+          {/* ---- PARA QUE SE CIERRE EL MODAL ---- */}
+          <Button title="Dismiss" onPress={() => onDismissModal(false)} />
+        </View>
+      </View>
+    </NewModal>
+  )
+}
+
+export default Modal
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalStyle: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 40,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalTextStyle: {
+    fontSize: 30,
+  },
+})
